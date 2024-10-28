bb16166fb14032f6d6b2d7e68be04d72ec7fe337
blsage <ben@sage.me>
2024-10-28 16:09:29 -0700

    Fixed token

--- server/generateToken.ts
+++ server/generateToken.ts
@@ -4,7 +4,7 @@ dotenv.config();
 import jwt from 'jsonwebtoken';
 
 const token = jwt.sign({ username: 'testUser' }, process.env.JWT_SECRET as string, {
-  expiresIn: '1w',
+  expiresIn: '1m',
 });
 
 console.log(token);
