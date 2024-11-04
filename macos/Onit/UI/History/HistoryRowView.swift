56f2a1dae28d2557ef612019e112bf44263f774b
blsage <ben@sage.me>
2024-11-04 01:01:25 -0500

    New app version

---
+++ macos/Onit/UI/History/HistoryRowView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryRowView.swift
+//  Onit
+//
+//  Created by Benjamin Sage on 11/4/24.
+//
+
+import SwiftUI
+
+struct HistoryRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryRowView()
+}
---
+++ macos/Onit/UI/History/HistorySearchView.swift
@@ -0,0 +1,18 @@
+//
+//  HistorySearchView.swift
+//  Onit
+//
+//  Created by Benjamin Sage on 11/4/24.
+//
+
+import SwiftUI
+
+struct HistorySearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistorySearchView()
+}
