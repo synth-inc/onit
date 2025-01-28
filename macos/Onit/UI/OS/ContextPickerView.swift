b104cd0ac8f6b03df4e2aa4c6597efc0a5e41a93
Niduank <naudin.k@gmail.com>
2025-01-28 23:19:32 +0100

    Code cleaning
    * Moved ContextPickerItemView to another file

--- macos/Onit/UI/OS/ContextPickerView.swift
+++ macos/Onit/UI/OS/ContextPickerView.swift
@@ -16,7 +16,7 @@ struct ContextPickerView: View {
                 model.closeContextPickerOverlay()
                 model.showFileImporter = true
             }) {
-                ContextView(imageRes: .file, title: "Upload file", subtitle: "Choose from computer")
+                ContextPickerItemView(imageRes: .file, title: "Upload file", subtitle: "Choose from computer")
             }
             .padding(.top, 6)
             .buttonStyle(.plain)
@@ -25,7 +25,7 @@ struct ContextPickerView: View {
                 model.closeContextPickerOverlay()
                 model.addAutoContext()
             }) {
-                ContextView(imageRes: .stars, title: "Auto-context", subtitle: "Current window activity")
+                ContextPickerItemView(imageRes: .stars, title: "Auto-context", subtitle: "Current window activity")
             }
             .buttonStyle(.plain)
             .padding(.bottom, 6)
@@ -33,36 +33,4 @@ struct ContextPickerView: View {
         .background(Color(.gray600))
         .cornerRadius(12)
     }
-}
-
-struct ContextView: View {
-    
-    let imageRes: ImageResource
-    let title: String
-    let subtitle: String
-    
-    var body: some View {
-        HStack(spacing: 0) {
-            Image(imageRes)
-                .resizable()
-                .frame(width: 20, height: 20)
-                .padding(.leading, 12)
-            
-            VStack(alignment: .leading, spacing: 4) {
-                Text(title)
-                    .appFont(.medium14)
-                    .foregroundColor(.white)
-                Text(subtitle)
-                    .appFont(.medium13)
-                    .foregroundColor(.gray200)
-            }
-            .padding(.leading, 8)
-            .padding(.vertical, 6)
-        }
-        .frame(minWidth: 210, alignment: .leading)
-    }
-}
-
-#Preview {
-    ContextPickerView()
 }
