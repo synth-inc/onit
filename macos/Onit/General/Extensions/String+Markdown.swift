390bc64d3d38511910a118f9759cfecb885012b9
Niduank <naudin.k@gmail.com>
2025-01-30 13:17:02 +0100

    Added specific support of Code, List and Heading

--- macos/Onit/General/Extensions/String+Markdown.swift
+++ macos/Onit/General/Extensions/String+Markdown.swift
@@ -14,7 +14,10 @@ extension String {
         let document = Document(parsing: self)
         var plainText = document.plainText
         
-        plainText.removeLast()
+        // Remove the last '\n'
+        if plainText.last == "\n" {
+            plainText.removeLast()
+        }
         
         return plainText
     }
@@ -35,18 +38,57 @@ extension Markup {
         switch self {
         case is Paragraph:
             return children.map { $0.plainText }.joined() + "\n"
-        case let text as Text:
-            return text.string
+            
         case is LineBreak:
             return "\n"
         case is SoftBreak:
             return " "
-        case is InlineCode, is InlineHTML:
-            return ""
+        
+        case let codeBlock as CodeBlock:
+            let code = codeBlock.code
+            return "\n\(code)\n"
+        
+        case let inlineCode as InlineCode:
+            return inlineCode.code
+            
+        case let unorderedList as UnorderedList:
+            return formatUnorderedList(unorderedList)
+            
+        case let orderedList as OrderedList:
+            return formatOrderedList(orderedList)
+        
+        case let heading as Heading:
+            return heading.children.map { $0.plainText }.joined() + "\n"
+            
+        case let text as Text:
+            return text.string
         case let link as Link:
             return link.children.map { $0.plainText }.joined()
         default:
             return children.map { $0.plainText }.joined()
         }
+    }
+    
+    private func formatUnorderedList(_ list: UnorderedList) -> String {
+        var result = ""
+        
+        for item in list.listItems {
+            let itemText = item.children.map { $0.plainText }.joined()
+            
+            result += "• \(itemText)"
+        }
+        
+        return result
+    }
+    
+    private func formatOrderedList(_ list: OrderedList) -> String {
+        var result = ""
+        
+        for (index, item) in list.listItems.enumerated() {
+            let itemText = item.children.map { $0.plainText }.joined()
+            result += "\(index + 1). \(itemText)"
+        }
+        
+        return result
     }
 }
