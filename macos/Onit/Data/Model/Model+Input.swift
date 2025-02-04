48fcc47ac20a07a0b6f836d7aeb4e613b74a5d78
Niduank <naudin.k@gmail.com>
2025-02-04 17:39:08 +0100

    comments

--- macos/Onit/Data/Model/Model+Input.swift
+++ macos/Onit/Data/Model/Model+Input.swift
@@ -10,9 +10,13 @@ import AppKit
 extension OnitModel {
     func addAutoContext() {
         guard FeatureFlagManager.shared.accessibility,
-              FeatureFlagManager.shared.accessibilityAutoContext,
-              let appName = AccessibilityNotificationsManager.shared.screenResult.applicationName,
+              FeatureFlagManager.shared.accessibilityAutoContext else {
+            return
+        }
+        
+        guard let appName = AccessibilityNotificationsManager.shared.screenResult.applicationName,
               let appContent = AccessibilityNotificationsManager.shared.screenResult.others else {
+            // TODO: KNA - notify user for empty context
             return
         }
         
@@ -24,7 +28,7 @@ extension OnitModel {
             return false
         }
         guard !contextDuplicated else {
-            // TODO: KNA - Display duplicated context to user
+            // TODO: KNA - Notify user for duplicated context
             return
         }
         
