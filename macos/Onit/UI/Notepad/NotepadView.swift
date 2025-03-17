13c4beafd7c39dcf6a785942524228ffe10ff14a
Niduank <naudin.k@gmail.com>
2025-03-17 10:37:54 +0100

    fix

--- macos/Onit/UI/Notepad/NotepadView.swift
+++ macos/Onit/UI/Notepad/NotepadView.swift
@@ -8,58 +8,19 @@
 import SwiftUI
 
 struct NotepadView: View {
-    @Environment(\.model) private var model
-    
     let closeCompletion: (() -> Void)
     
-    @Binding var prompt: Prompt?
+    @EnvironmentObject var config: NotepadConfig
     
-    init(prompt: Binding<Prompt?> = .constant(nil), closeCompletion: @escaping () -> Void) {
-        self._prompt = prompt
+    init(closeCompletion: @escaping () -> Void) {
         self.closeCompletion = closeCompletion
-    }
-    
-    private var oldText: String {
-        guard let prompt = prompt else { return "" }
-        
-        let autoContexts = prompt.contextList.autoContexts
-        
-        if let input = prompt.input {
-            print("NotepadView oldText : \(input.selectedText)")
-            return input.selectedText
-        } else if !autoContexts.isEmpty {
-            print("NotepadView oldText : \(autoContexts.values.joined(separator: "\n"))")
-            return autoContexts.values.joined(separator: "\n")
-        }
-        
-        return ""
-    }
-    private var newText: String {
-        guard let prompt = prompt else { return "" }
-        
-        let response = prompt.responses[prompt.generationIndex]
-        
-        print("NotepadView newText : \(response.isPartial ? model.streamedResponse : response.text)")
-        
-        return response.isPartial ? model.streamedResponse : response.text
-    }
-    private var isStreaming: Bool {
-        guard let prompt = prompt else {
-            return false
-        }
-        
-        let response = prompt.responses[prompt.generationIndex]
-        
-        print("NotepadView isStreaming : \(response.isPartial)")
-        
-        return response.isPartial
     }
     
     var body: some View {
         VStack(spacing: 0) {
             toolbar
             PromptDivider()
-            DiffView(oldText: oldText, newText: newText, isStreaming: isStreaming)
+            DiffView(oldText: $config.oldText, newText: $config.newText, isStreaming: $config.isStreaming)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
         }
     }
@@ -84,7 +45,8 @@ struct NotepadView: View {
 }
 
 #Preview {
-    NotepadView(prompt: .constant(nil)) {
+    NotepadView {
         
     }
+    .environmentObject(NotepadConfig())
 }
