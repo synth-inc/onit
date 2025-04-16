779b801cfe0272d2db54c28842273a79d1e90ab1
Niduank <naudin.k@gmail.com>
2025-04-16 11:17:13 +0200

    Logging - Implement SwiftyBeaver

--- macos/Onit/AppDelegate.swift
+++ macos/Onit/AppDelegate.swift
@@ -8,11 +8,15 @@
 import FirebaseCore
 import PostHog
 import SwiftUI
+import SwiftyBeaver
+
+let log = SwiftyBeaver.self
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         FirebaseApp.configure()
+        configureSwiftBeaver()
 
         // This is helpful for debugging the new user experience, but should never be committed!
         //        if let appDomain = Bundle.main.bundleIdentifier {
@@ -23,5 +27,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_ notification: Notification) {
         PostHogSDK.shared.capture("app_quit")
+    }
+    
+    private func configureSwiftBeaver() {
+        #if DEBUG
+        let logsDirectory = FileManager.default.urls(for: .libraryDirectory, in: .userDomainMask).first!
+        let logFileURL = logsDirectory.appendingPathComponent("Logs/Onit.log")
+        
+        let file = FileDestination(logFileURL: logFileURL)
+        let console = ConsoleDestination()
+        
+        log.addDestination(console)
+        log.addDestination(file)
+        #endif
     }
 }
