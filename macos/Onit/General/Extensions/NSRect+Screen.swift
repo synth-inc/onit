3cd6d29e1ab5ef89987412989ad5aa86fe275897
timl <tim.lenardo@gmail.com>
2025-04-16 19:19:28 -0700

    update findScreen implementation to find teh screen that contains the most overlap with the window

--- macos/Onit/General/Extensions/NSRect+Screen.swift
+++ macos/Onit/General/Extensions/NSRect+Screen.swift
@@ -27,21 +27,13 @@ extension NSRect {
     }
     
     func findScreen() -> NSScreen? {
-        guard let mainScreen = NSScreen.main else {
-            return nil
-        }
-        let matchingScreen = NSScreen.screens
-            .filter({ $0 == mainScreen })
-            .first(where: { origin.x >= $0.frame.minX && origin.x < $0.frame.maxX })
-        
-        if origin.y < 0 || origin.y > mainScreen.frame.maxY ||
-            origin.x < 0 || origin.x >= mainScreen.frame.maxX {
-            if let matchingScreen = matchingScreen {
-                return matchingScreen
-            }
+        let matchingScreen = NSScreen.screens.max { (screen1, screen2) -> Bool in
+            let intersection1 = screen1.frame.intersection(self)
+            let intersection2 = screen2.frame.intersection(self)
+            return intersection1.width * intersection1.height < intersection2.width * intersection2.height
         }
         
-        return mainScreen
+        return matchingScreen ?? NSScreen.main
     }
     
     func calculateWindowDistanceFromBottom() -> CGFloat? {
