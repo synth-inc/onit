a6856e4ff7c699f2eaf55c6be44285faef954e8a
Jay Swanson <jay@synth.inc>
2025-05-15 15:02:30 -0700

    Use newer Apple tokenizer

--- macos/Onit/Helpers/Chunker.swift
+++ macos/Onit/Helpers/Chunker.swift
@@ -6,32 +6,18 @@
 //
 
 import Foundation
+import NaturalLanguage
 
 public class Chunker {
-    public static func chunkByUnit(_ text: String, unit: NSLinguisticTaggerUnit) -> [String] {
-        let tagger = NSLinguisticTagger(tagSchemes: [.tokenType], options: 0)
-        tagger.string = text
+    public static func chunkByUnit(_ text: String, unit: NLTokenUnit) -> [String] {
+        let tokenizer = NLTokenizer(unit: unit)
+        tokenizer.string = text
         
-        var chunks: [String] = []
-        
-        let range = NSRange(location: 0, length: text.utf16.count)
-        
-        tagger.enumerateTags(in: range, unit: unit, scheme: .tokenType, options: []) { _, tokenRange, _ in
-            if let chunk = text.substring(with: tokenRange) {
-                let cleanedChunk = chunk.trimmingCharacters(in: .whitespacesAndNewlines)
-                if !cleanedChunk.isEmpty {
-                    chunks.append(cleanedChunk)
-                }
-            }
+        var tokens: [String] = []
+        tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { (tokenRange, _) -> Bool in
+            tokens.append(String(text[tokenRange]))
+            return true
         }
-        
-        return chunks
-    }
-}
-
-private extension String {
-    func substring(with nsRange: NSRange) -> String? {
-        guard let range = Range(nsRange, in: self) else { return nil }
-        return String(self[range])
+        return tokens
     }
 }
