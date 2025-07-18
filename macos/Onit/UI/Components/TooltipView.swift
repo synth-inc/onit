02a285900d363ec102be57fa05ab00229f3b93ab
lk340 <loyd.hkim@gmail.com>
2025-07-18 17:54:04 -0400

    Fix multi-line text truncation

--- macos/Onit/UI/Components/TooltipView.swift
+++ macos/Onit/UI/Components/TooltipView.swift
@@ -10,102 +10,79 @@ import SwiftUI
 
 struct TooltipConfig {
     var maxWidth: CGFloat
-    var lineLimit: Int? = nil
 }
 
 struct TooltipView: View {
     var tooltip: Tooltip
     var config: TooltipConfig? = nil
+    
+    private var text: String {
+        let trimmedTooltipText = tooltip.prompt.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        let textSubstrings: [String] = trimmedTooltipText.components(separatedBy: CharacterSet.newlines)
+        let textSubstringsCount = textSubstrings.count
+        
+        if textSubstringsCount > 1 {
+            let maxIndex = textSubstringsCount > 4 ? 4 : textSubstringsCount
+            var trimmedTextSubstrings = textSubstrings[0..<maxIndex].joined(separator: "\n")
+            
+            if trimmedTextSubstrings.count > 200 {
+                trimmedTextSubstrings = String(trimmedTextSubstrings.prefix(200)) + "..."
+            }
+            
+            if textSubstringsCount > 4 {
+                trimmedTextSubstrings += "\n..."
+            }
+            
+            return trimmedTextSubstrings
+        } else {
+            return trimmedTooltipText
+        }
+    }
 
     var body: some View {
-        if let config = config {
-            truncatedTextView(config)
-        } else {
-            fixedTextView
-        }
-    }
-    
-    // MARK: - Child Components
-    
-    private func findValidSubstring(_ substrings: [String]) -> String {
-        return substrings
-            .first { !$0.trimmingCharacters(in: .whitespaces).isEmpty }?
-            .trimmingCharacters(in: .whitespaces)
-        ?? ""
-    }
-    @ViewBuilder
-    var textView: some View {
-        let newlineCharacters = CharacterSet.newlines
-        let textSubstrings: [String] = tooltip.prompt.components(separatedBy: newlineCharacters)
-        let textFirstLine: String = findValidSubstring(textSubstrings)
-        
-        let hasMultipleLines = textSubstrings.count > 1
-        
-        if hasMultipleLines {
-            VStack(alignment: .leading) {
-                Text(textFirstLine)
-                Text("...")
-            }
-            .padding(.vertical, 8)
-            .styleText(size: 12)
-        } else {
-            Text(textFirstLine)
-                .padding(.vertical, 8)
-                .styleText(size: 12)
-        }
-    }
-    
-    func textWithShortcutWrapper(_ textView: some View) -> some View {
         HStack(spacing: 0) {
-            textView
-            tooltipShortcut
+            if let config = config {
+                textView
+                    .frame(
+                        maxWidth: config.maxWidth,
+                        alignment: .leading
+                    )
+            } else {
+                textView
+            }
+
+            Group {
+                switch tooltip.shortcut {
+
+                case .keyboardShortcuts(let name):
+                    if let shortcut = KeyboardShortcuts.getShortcut(for: name) {
+                        KeyboardShortcutView(shortcut: shortcut.native)
+                            .padding(.horizontal, 4)
+                            .padding(.vertical, 6)
+                            .background(.gray400, in: .rect(cornerRadius: 6))
+                            .padding(4)
+                            .fixedSize()
+                    }
+                case .none:
+                    Spacer()
+                        .frame(width: 8)
+                }
+            }
+            .appFont(.medium10)
         }
         .foregroundStyle(.white)
         .padding(.leading, 8)
         .background {
             tooltipBackground
         }
-    }
-    
-    var fixedTextView: some View {
-        textWithShortcutWrapper(
-            textView
-                .fixedSize()
-        )
         .frame(minHeight: 78)
     }
     
-    func truncatedTextView(_ config: TooltipConfig) -> some View {
-        textWithShortcutWrapper(
-            textView
-                .truncateText(lineLimit: config.lineLimit ?? 6)
-                .frame(
-                    maxWidth: config.maxWidth,
-                    alignment: .leading
-                )
-        )
-        .fixedSize(horizontal: false, vertical: true)
-    }
-    
-    var tooltipShortcut: some View {
-        Group {
-            switch tooltip.shortcut {
-
-            case .keyboardShortcuts(let name):
-                if let shortcut = KeyboardShortcuts.getShortcut(for: name) {
-                    KeyboardShortcutView(shortcut: shortcut.native)
-                        .padding(.horizontal, 4)
-                        .padding(.vertical, 6)
-                        .background(.gray400, in: .rect(cornerRadius: 6))
-                        .padding(4)
-                        .fixedSize()
-                }
-            case .none:
-                Spacer()
-                    .frame(width: 8)
-            }
-        }
-        .appFont(.medium10)
+    var textView: some View {
+        Text(text)
+            .appFont(.medium12)
+            .padding(.vertical, 8)
     }
     
     var tooltipBackground: some View {
